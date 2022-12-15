48bea1d004a6b45da5fbcfaac96d7b45e814c8aa
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2022-12-16 00:02:25 +0800

    add: samba setup and pgsql rule doc

---
+++ my-config/javavars.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem java env
+set JAVA_HOME=C:\Java\jdk1.8.0_121
+set PATH=%PATH%;%JAVA_HOME%\bin
+rem set CLASSPATH=.;%JAVA_HOME%\lib;%JAVA_HOME%\lib\tools.jar
+
+rem scala env
+rem set SCALA_HOME=C:\Java\scala-2.11.8
+rem set PATH=%PATH%;%SCALA_HOME%\bin
+
+rem groovy env
+set GROOVY_HOME=C:\Java\groovy-2.4.9
+set PATH=%PATH%;%GROOVY_HOME%\bin
+
+rem gradle env
+set GRADLE_HOME=C:\Java\gradle-3.4.1
+set PATH=%PATH%;%GRADLE_HOME%\bin
+
